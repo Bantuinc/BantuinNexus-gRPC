f48b8e80e083bd90cd9705e651a5f9a027763e18
hubedid <hubed@protonmail.com>
2023-07-11 12:26:35 +0700

    [MOD] Add Authorization & QueryFactory

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using BantuinNexus_gRPC;
 using BantuinNexus_gRPC.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using MySql.Data.MySqlClient;
+using SqlKata.Compilers;
+using SqlKata.Execution;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,12 +12,33 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(SettingsConfigHelper.AppSetting("Jwt:SecretKey")))
+        };
+    });
+builder.Services.AddAuthorization();
 builder.Services.AddGrpc();
+builder.Services.AddTransient<QueryFactory>((e) =>
+{
+    var connection = new MySqlConnection(SettingsConfigHelper.AppSetting("ConnectionStrings:MySql"));
+    var compiler = new MySqlCompiler();
+    return new QueryFactory(connection, compiler);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<AccountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
